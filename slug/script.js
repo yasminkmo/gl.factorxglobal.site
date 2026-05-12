d050aa37ce7e46b47e206cb364d80c5a00d2cfb2
yasminkmo <minvds22@gmail.com>
2026-05-12 19:36:19 -0300

    Update script.js

--- slug/script.js
+++ slug/script.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // ⏱️ Tempo de Delay: Formato "Minutos:Segundos"
-    tempoDeDelay: "30:00",
+    tempoDeDelay: "00:02",
 
     // 🔗 Links de Checkout dos Botões
     linkPote2: "https://magazinetype2.online/b?p=GLPPT2V1&b=132&fid=472&fnid=2&pfnid=1&pg=8395&aff_id=241183",
